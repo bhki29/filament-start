d52a1958e01e46d66d2119964660e020fb295302
bhki29 <ajiahmadbaehaki20@gmail.com>
2025-05-01 14:57:42 +0700

    create Lapora penjualan section

---
+++ app/Filament/Resources/PenjualanResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PenjualanResource\Pages;
+use App\Filament\Resources\PenjualanResource\RelationManagers;
+use App\Models\PenjualanModel;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Filament\Tables\Columns\TextColumn;
+
+class PenjualanResource extends Resource
+{
+    protected static ?string $model = PenjualanModel::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-chart-bar';
+
+    protected static ?string $navigationLabel = 'Laporan Penjualan';
+
+    protected static ?string $navigationGroup = 'Faktur';
+
+    protected static ?string $label = 'Laporan Penjualan';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('tanggal')
+                ->label('Tanggal')
+                ->sortable()
+                ->searchable()
+                ->date('d F Y'),
+                TextColumn::make('kode')
+                ->label('Kode Faktur')
+                ->sortable()
+                ->searchable(),
+                TextColumn::make('jumlah')
+                ->label('Jumlah')
+                ->sortable()
+                ->searchable(),
+                TextColumn::make('customer.nama_customer')
+                ->label('Nama Customer')
+                ->sortable()
+                ->searchable(),
+                TextColumn::make('status')
+                ->label('Status')
+                ->sortable()
+                ->searchable()
+                ->badge(),
+                TextColumn::make('jenis')
+                ->label('Jenis')
+                ->sortable()
+                ->searchable()
+                ->badge(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPenjualans::route('/'),
+            'create' => Pages\CreatePenjualan::route('/create'),
+            'edit' => Pages\EditPenjualan::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/PenjualanModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PenjualanModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'penjualan';
+
+    public function customer()
+    {
+        return $this->belongsTo(CustomerModel::class);
+    }
+
+    public function faktur()
+    {
+        return $this->belongsTo(FakturModel::class, 'id');
+    }
+}
